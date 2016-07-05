2c01e7b0274306c00ace4623f76cfff10c7d2885
wfdd <erdbanfed@gmail.com>
2016-07-05 15:40:14 +0300

    Scrape the previous term

--- scraper.rb
+++ scraper.rb
@@ -5,11 +5,20 @@ require 'scraperwiki'
 require 'capybara'
 require 'capybara/poltergeist'
 
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(
+    app,
+    # Submit button's onClick references an undefined variable, 'btn_OK'
+    {js_errors: false, phantomjs_options: ['--load-images=no']}
+  )
+end
+
 include Capybara::DSL
 Capybara.default_driver = :poltergeist
 
-def scrape(pageno)
-  warn "Scraping page #{pageno}"
+def scrape(term, pageno = 1)
+  warn "Scraping page #{pageno} of the #{term}th term"
   table = page.find_by_id('ctl00_ContentPlaceHolder1_GridView1')
   table.all(:xpath, './tbody/tr[position() > 1 and position() < last()]').each do |mp|
     tds = mp.all(:xpath, './td')
@@ -21,7 +30,7 @@ def scrape(pageno)
       party: tds[1].text.strip,
       party_id: tds[1].text.strip,
       area: tds[2].text.strip,
-      term: 9,
+      term: term,
       source: "http://www.parlamento.cv/#{link[:href]}",
     }
     ScraperWiki.save_sqlite([:id, :term], data)
@@ -37,5 +46,17 @@ def scrape(pageno)
   end
 end
 
-visit 'http://www.parlamento.cv/deputados2.aspx'
-scrape(1)
+def main
+  visit 'http://www.parlamento.cv/deputados2.aspx'
+  scrape(9)
+
+  # Reloading the web page to return to the 1st page of the table 'cause it's
+  # easier that way
+  visit 'http://www.parlamento.cv/deputados2.aspx'
+  sentinel = page.find_by_id('ctl00_ContentPlaceHolder1_GridView1').all('tr').last
+  page.find_by_id('ctl00_ContentPlaceHolder1_DropDownList1').select('VIII')
+  page.find_by_id('ctl00_ContentPlaceHolder1_btnOK').click
+  {} while sentinel.visible? rescue scrape(8)
+end
+
+main
